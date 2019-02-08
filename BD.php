14cd5a29106b376286389ba23a65faa3721c3aab
Alonso <dragon-alonso@hotmail.com>
2019-02-07 20:37:41 -0600

    1.-Se reestructuro el funcionamiento de ScriptPhp BD.php. 2.-Se elimino el archivo insertar.php. 3.- Se reestructuro el archivo index para utilizar las funciones de Insertar,Consultar, Eliminar y Actualizar

--- BD.php
+++ BD.php
@@ -3,4 +3,6 @@
  		$contrasena = "Resident";  
  		$servidor = "localhost";
 		$basededatos = "Productos";
+
+		$conexion = mysqli_connect( $servidor, $usuario, $contrasena, $basededatos)or die ("No se ha podido conectar al servidor de Base de datos");
 ?>
--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 
 <body>
 	 
-		<div id="div1" >
+	<div id="div1" >
 		<div id="div2">
 
 			<div id="nav_wrapper">
@@ -31,46 +31,129 @@
 			</div>
 
 		</div>
+<div class="col-md-4">
 
-<div id="div3"><h1>Bienvenido</h1>
-	<h1>Escuelita Magica</h1><p>Por Excelencia Academica</p>
-	<p>Valor y Responsabilidad Social</p>
+    <center><h1>PROPIETARIO</h1></center>
 
-	<form action="insertar.php" method="POST" name="form">
-		<input type="text" name="ID"><br/>
-		<input type="text" name="MARCA"><br/>
-		<input type="text" name="PRECIO"><br/>
-		<input type="submit" value="insertar datos" class="" name="btn1">
-		<input type="submit" value="consultar" class="" name="btn2">
-		<input type="submit" value="actualizar" class="" name="btn3">
-		<input type="submit" value="eliminar" class="" name="btn4">
-	</form>
+    <form method="POST" action="index.php" >
 
-	
+    <div class="form-group">
+      <label for="ID">ID</label>
+      <input type="text" name="id" class="form-control" id="id">
+    </div>
+    <br>
+    <div class="form-group">
+        <label for="marca">Marca</label>
+        <input type="text" name="marca" class="form-control" id="marca" >
+    </div>
+    <br>
+    <div class="form-group">
+        <label for="precio">Precio</label>
+        <input type="text" name="precio" class="form-control" id="precio">
+    </div>
+    <br>
+    <div class="form-group">
+        <label for="unidades">Unidades</label>
+        <input type="text" name="unidades" class="form-control" id="unidades">
+    </div>
+    <br>
+    <center>
+      <input type="submit" value="Registrar" class="btn btn-success" name="btn_registrar">
+      <input type="submit" value="Consultar" class="btn btn-primary" name="btn_consultar">
+      <input type="submit" value="Actualizar" class="btn btn-info" name="btn_actualizar">
+      <input type="submit" value="Eliminar" class="btn btn-danger" name="btn_eliminar">
+    </center>
 
+  </form>
+  <?php
+    include("BD.php");
 
-	<?php
-		/* Conexion pruebas inicio php*/
+      if(isset($_POST['btn_registrar']))
+      {
+	      $id   	 =$_POST['id'];
+	      $marca     =$_POST['marca'];
+	      $precio    =$_POST['precio'];
+	      $unidades  =$_POST['unidades'];
+	     	 if($id=="" || $marca=="" || $precio=="" || $unidades=="")
+      		{
+      				echo "Llene todos los espacios";
+     			 }
+     			 else
+     			 {
+     		 		mysqli_query($conexion, "INSERT INTO productos (ID,Marca,Precio,Unidades) values ('$id','$marca','$precio','$unidades')");
+      				echo "Se ha agredado correctamente el registro";
+      				mysql_close($conexion);
+     			 }
+    		}
+     
+      if(isset($_POST['btn_consultar']))
+      {
+	       $id 		  = $_POST['id'];
+	       $marca     =$_POST['marca'];
+		   $precio    =$_POST['precio'];
+		   $unidades  =$_POST['unidades'];
+	       
+	       	if ($id=="") 
+	       	{
+	       		echo "Se debe de colocar el ID";
+	       	}
+	       	else
+	       	{
+	       		$resultados = mysqli_query($conexion,"SELECT * FROM productos WHERE ID = '$id'");
+	  			while($consulta = mysqli_fetch_array($resultados))
+  				{
+		    		echo $consulta['ID']."<br>";
+		    		echo $consulta['Marca']."<br>";
+		    		echo $consulta['Precio']."<br>";
+		    		echo $consulta['Unidades']."<br>";
+		  		}
+	       	}
+     
 
-		$usuario = "Alonso";
- 		$contrasena = "Resident";  
- 		$servidor = "localhost";
-		$basededatos = "Productos";
-		
+      }
 
-		$conexion = mysqli_connect( $servidor, $usuario, "Resident")or die ("No se ha podido conectar al servidor de Base de datos");
- 
- 		$db = mysqli_select_db( $conexion, $basededatos )  or die ( "Upps! Pues va a ser que no se ha podido conectar a la base de datos" );
+      if(isset($_POST['btn_actualizar']))
+      {
+	      $id   	 =$_POST['id'];
+	      $marca     =$_POST['marca'];
+	      $precio    =$_POST['precio'];
+	      $unidades  =$_POST['unidades'];
+	     	 if($id=="" || $marca=="" || $precio=="" || $unidades=="")
+      		{
+      			echo "Llene todos los espacios";
+     		}
+     		else
+     		{
+     		 	$_UPDATE_SQL="UPDATE productos Set ID='$id', Marca='$marca', Precio='$precio', Unidades='$unidades' WHERE ID='$id'"; 
+  				mysqli_query($conexion,$_UPDATE_SQL); 
+      			echo "Se ha Actualizado correctamente";
+     		}
+      }
 
-		
-		mysqli_close( $conexion );
-	?>	
-	</div>
- */
+      if(isset($_POST['btn_eliminar']))
+      {
+        $id = $_POST['id'];
+        if($id=="")
+        {
+        	echo "El campo ID es obligatorio";
+        }
+        else
+        {
+        	$_DELETE_SQL =  "DELETE FROM productos WHERE ID = '$id'";
+  			mysqli_query($conexion,$_DELETE_SQL); 
+  			echo "Se ha eliminado el registro";
+ 				
+        }
+
+        
+      }
+
+   
+  ?>
 </div>
 
-	
 
-	</div>
+</div>
+		
 </body>
 </html>
