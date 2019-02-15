da35e4f2779559a41bad086e6acde56d02bfcd0d
Alonso <dragon-alonso@hotmail.com>
2019-02-15 14:04:56 -0600

    Implementacion de cambios en el index.php y procesos.php

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
 		<div class="col-md-4">
 		<center><h1>PROPIETARIO</h1></center>
 
-		<form method="POST" action="index.php?action=submit">
+		<form method="POST" action="procesos.php?action=submit">
 			<div class="form-group">
 				<label for="ID">ID</label>
 				<input type="text" name="id" class="form-control" id="id">
@@ -67,77 +67,7 @@
 		 * CRUD (Create, Read, Update, Delete)
 		 */
 			//Desde aqui se movieron los procesos a procesos.php
-		// Create
-		if (isset($_POST['create'])) {
-			// importante: convertir a entero con (int), previene sql injection
-			$id = (int)$_POST['id'];
-			$marca = $_POST['marca'];
-			$precio = $_POST['precio'];
-			$unidades = $_POST['unidades'];
-
-			if ($id=="" || $marca=="" || $precio=="" || $unidades=="") {
-				echo "Llene todos los espacios";
-			} else {
-				if (!mysqli_query($conexion, "INSERT INTO productos (id, marca, precio, unidades) values ('$id', '$marca', '$precio', '$unidades')")) {
-					echo("Hubo un error: ".mysqli_error($conexion));
-				} else {
-					echo "Se ha agredado correctamente el registro";
-				}
-			}
-		}
-
-		// Read
-		if (isset($_POST['read'])) {
-			// importante: convertir a entero con (int), previene sql injection
-			$id = (int)$_POST['id'];
-			$marca = $_POST['marca'];
-			$precio = $_POST['precio'];
-			$unidades = $_POST['unidades'];
-
-			if ($id=="") {
-				echo "Se debe de colocar el ID";
-			} else {
-				$resultados = mysqli_query($conexion, "SELECT * FROM productos WHERE id = ".$id);
-
-				while ($consulta = mysqli_fetch_array($resultados)) {
-					echo $consulta['id']."<br>";
-					echo $consulta['marca']."<br>";
-					echo $consulta['precio']."<br>";
-					echo $consulta['unidades']."<br>";
-				}
-			}
-		}
-
-		// Update
-		if (isset($_POST['update'])) {
-			// importante: convertir a entero con (int), previene sql injection
-			$id = (int)$_POST['id'];
-			$marca = $_POST['marca'];
-			$precio = $_POST['precio'];
-			$unidades = $_POST['unidades'];
-			if ($id=="" || $marca=="" || $precio=="" || $unidades=="") {
-				echo "Llene todos los espacios";
-			} else {
-				if (!mysqli_query($conexion, "UPDATE productos SET marca='$marca', precio='$precio', unidades='$unidades' WHERE id='$id'")) {
-					echo "Error: ".mysqli_error($conexion);
-				} else {
-					echo "Se ha Actualizado correctamente";
-				} 
-			}
-		}
-
-		if (isset($_POST['delete'])) {
-			$id = (int)$_POST['id'];
-			if ($id=="") {
-				echo "El campo ID es obligatorio";
-			} else {
-				if (!mysqli_query($conexion, "DELETE FROM productos WHERE id = '$id'")) {
-					echo "Error: ".mysqli_error($conexion);
-				} else {
-					echo "Se ha eliminado el registro";
-				}
-			}
-		}
+		
 
 		// cerrar conexion
 		if ($_GET['action'] == 'submit') {
--- procesos.php
+++ procesos.php
@@ -1,9 +1,85 @@
 <?php
+	include("BD.php");
 	
 	if(isset($_POST['id']) && !empty($_POST['id']) &&
 		isset($_POST['marca']) && !empty($_POST['marca']) &&
 		isset($_POST['precio']) && !empty($_POST['precio']) &&
 		isset($_POST['unidades']) && !empty($_POST['unidades']) )
+
+	{
+
+		// Create
+		if (isset($_POST['create'])) {
+			// importante: convertir a entero con (int), previene sql injection
+			$id = (int)$_POST['id'];
+			$marca = $_POST['marca'];
+			$precio = $_POST['precio'];
+			$unidades = $_POST['unidades'];
+
+			if ($id=="" || $marca=="" || $precio=="" || $unidades=="") {
+				echo "Llene todos los espacios";
+			} else {
+				if (!mysqli_query($conexion, "INSERT INTO productos (id, marca, precio, unidades) values ('$id', '$marca', '$precio', '$unidades')")) {
+					echo("Hubo un error: ".mysqli_error($conexion));
+				} else {
+					echo "Se ha agredado correctamente el registro";
+				}
+			}
+		}
+
+		// Read
+		if (isset($_POST['read'])) {
+			// importante: convertir a entero con (int), previene sql injection
+			$id = (int)$_POST['id'];
+			$marca = $_POST['marca'];
+			$precio = $_POST['precio'];
+			$unidades = $_POST['unidades'];
+
+			if ($id=="") {
+				echo "Se debe de colocar el ID";
+			} else {
+				$resultados = mysqli_query($conexion, "SELECT * FROM productos WHERE id = ".$id);
+
+				while ($consulta = mysqli_fetch_array($resultados)) {
+					echo $consulta['id']."<br>";
+					echo $consulta['marca']."<br>";
+					echo $consulta['precio']."<br>";
+					echo $consulta['unidades']."<br>";
+				}
+			}
+		}
+
+		// Update
+		if (isset($_POST['update'])) {
+			// importante: convertir a entero con (int), previene sql injection
+			$id = (int)$_POST['id'];
+			$marca = $_POST['marca'];
+			$precio = $_POST['precio'];
+			$unidades = $_POST['unidades'];
+			if ($id=="" || $marca=="" || $precio=="" || $unidades=="") {
+				echo "Llene todos los espacios";
+			} else {
+				if (!mysqli_query($conexion, "UPDATE productos SET marca='$marca', precio='$precio', unidades='$unidades' WHERE id='$id'")) {
+					echo "Error: ".mysqli_error($conexion);
+				} else {
+					echo "Se ha Actualizado correctamente";
+				} 
+			}
+		}
+
+		if (isset($_POST['delete'])) {
+			$id = (int)$_POST['id'];
+			if ($id=="") {
+				echo "El campo ID es obligatorio";
+			} else {
+				if (!mysqli_query($conexion, "DELETE FROM productos WHERE id = '$id'")) {
+					echo "Error: ".mysqli_error($conexion);
+				} else {
+					echo "Se ha eliminado el registro";
+				}
+			}
+		}
+	}
 
 
 
